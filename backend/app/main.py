33be1a3bc232dab3ef9dad83610bc6ea1134d747
priyashmita <priyashmita@gmail.com>
2026-03-28 14:44:13 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,10 @@
+from fastapi import FastAPI
+from app.api import operations
 
+app = FastAPI()
+
+app.include_router(operations.router)
+
+@app.get("/")
+def root():
+    return {"message": "RAGE backend running"}
